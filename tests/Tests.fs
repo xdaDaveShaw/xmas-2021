ed372cc9d22385355d204cf581f1b3df1d8b9c3b
Dave Shaw <dave@taeguk.co.uk>
2021-12-02 21:05:09 +0000

    First test

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -1,8 +1,36 @@
 module Tests
 
-open System
+open site.App
+open System.Net.Http
 open Xunit
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.TestHost
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+
+
+let createTestHost () =
+  WebHostBuilder()
+    .UseTestServer()
+    .Configure(configureApp)
+    .ConfigureServices(configureServices)
+    
 
 [<Fact>]
 let ``My test`` () =
-    Assert.True(true)
+    task {
+        use server = new TestServer(createTestHost())
+
+        use msg = new HttpRequestMessage(HttpMethod.Get, "/")
+
+        use client = server.CreateClient()
+        use! response = client.SendAsync msg
+        let! content = response.Content.ReadAsStringAsync()
+
+        let expected = "hello test"
+
+        Assert.Equal(expected, content)
+    }
+    
